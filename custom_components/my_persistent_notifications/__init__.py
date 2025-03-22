80a69c8bd6f7dc3e552fb189b76222108083228e
reiniertc <reinier@taudin-chabot.nl>
2025-03-22 10:04:33 +0100

    Update __init__.py

--- custom_components/my_persistent_notifications/__init__.py
+++ custom_components/my_persistent_notifications/__init__.py
@@ -1,6 +1,1 @@
-"""Custom integration for Persistent Notifications."""
-import logging
-
-LOGGER = logging.getLogger(__name__)
-LOGGER.info("✅ my_persistent_notifications wordt geladen!")
-
+"""Initbestand voor persistent notification sensor."""
